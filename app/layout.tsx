94711c875a1e3a04ef767ad305fb73470c5aebbd
Sean Goodwin <dealappseo@gmail.com>
2025-12-27 23:12:22 -0800

    Add Analytics component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body className={`${spaceGrotesk.variable} ${inter.variable} font-sans antialiased`}>
+        
         {children}
         <Analytics />
       </body>
