b54705410fc0d40955ffc3b90843b33054e88cbd
Sean Goodwin <dealappseo@gmail.com>
2025-12-28 00:50:19 -0800

    Implement Content Security Policy in headers
    Added Content Security Policy headers to enhance security.

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self' data:; connect-src 'self' https:;",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
