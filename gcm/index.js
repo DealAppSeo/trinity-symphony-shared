44c3beafdbf61e418d99c89cb3081e00dac314c6
HyperDAG <dealappseo@gmail.com>
2026-01-03 17:01:49 -0800

    fix: inject ConstitutionalAgentV4 locally to fix module resolution on railway

--- gcm/index.js
+++ gcm/index.js
@@ -1,7 +1,7 @@
 /**
- * TRANSMISSION: GCM (V4 UNIFIED)
+ * TRANSMISSION: GCM (V4 INJECTED)
  */
-const ConstitutionalAgentV4 = require('../lib/ConstitutionalAgentV4');
+const ConstitutionalAgentV4 = require('./ConstitutionalAgentV4');
 
 const agent = new ConstitutionalAgentV4({
   name: process.env.AGENT_NAME || 'GCM'
--- mel/index.js
+++ mel/index.js
@@ -1,7 +1,7 @@
 /**
- * TRANSMISSION: MEL (V4 UNIFIED)
+ * TRANSMISSION: MEL (V4 INJECTED)
  */
-const ConstitutionalAgentV4 = require('../lib/ConstitutionalAgentV4');
+const ConstitutionalAgentV4 = require('./ConstitutionalAgentV4');
 
 const agent = new ConstitutionalAgentV4({
   name: process.env.AGENT_NAME || 'MEL'
